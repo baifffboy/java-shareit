bb9a2b9a9db637c3d17dae785ad7ccdae765b774
baifffboy <ilya.yakimov.43@gmail.com>
2026-06-27 00:12:24 +0300

    minor change

--- src/main/java/ru/practicum/shareit/booking/service/BookingServiceImpl.java
+++ src/main/java/ru/practicum/shareit/booking/service/BookingServiceImpl.java
@@ -10,6 +10,7 @@ import ru.practicum.shareit.booking.dto.UpdateBookingRequest;
 import ru.practicum.shareit.booking.mapper.BookingMapper;
 import ru.practicum.shareit.booking.model.Booking;
 import ru.practicum.shareit.booking.model.Status;
+import ru.practicum.shareit.exception.ForbiddenException;
 import ru.practicum.shareit.exception.NotFoundException;
 import ru.practicum.shareit.exception.ValidationException;
 import ru.practicum.shareit.item.dao.ItemRepositoryInDatabase;
@@ -57,8 +58,8 @@ public class BookingServiceImpl implements BookingService {
             throw new NotFoundException("Пользователь с данным id не существует");
         Optional<Booking> booking = bookingRepository.findById(bookingId);
         if (booking.isPresent()) {
-            if (!booking.get().getItem().getOwner().equals(userRepository.findById(userId).get()))
-                throw new NotFoundException("Одобрить бронирование может только владелец данной вещи");
+            if (!booking.get().getItem().getOwner().getId().equals(userId))
+                throw new ForbiddenException("Одобрить бронирование может только владелец данной вещи");
         } else throw new ValidationException("Некорректно введено id бронирования");
         UpdateBookingRequest updateBookingRequest = new UpdateBookingRequest();
         updateBookingRequest.setId(bookingId);
